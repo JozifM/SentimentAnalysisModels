0b5403797e825a869285ee87bf07941e401cc65f
JozifM <Joewilliammurray@gmail.com>
2023-05-17 20:35:19 +0100

    Uploaded models and params

---
+++ params.py
@@ -0,0 +1,7 @@
+credentials = {
+    'API_key': '1WWzfrzZkllHeHe8Xe9pLKgaZ',
+    'API_secret': 'DDgFyedOdzb3y1TNVd6H04ozezWNmy6FRQ9vUgZSh6bbtwbVK1',
+    'bearer_token': 'AAAAAAAAAAAAAAAAAAAAAN2RiwEAAAAALpFlikodoMH0swHQazp9HKsfNaQ%3DaK0D7dYyDXX1sgri2MLj5d5DkfNofDImi3Qjg1NYr1SIAMz1kF',
+    'access_token': '832016410436071424-ZiJ94HTKFxH85GaVtyRgtBC6zOKjbBr',
+    'access_secret': 'aC9iDhxSeKKMMChlupf1EQ7CpdC9QXphSuVrfuDx58Xsr',
+}
